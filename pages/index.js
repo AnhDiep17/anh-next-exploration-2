8c988d5af6d354056a1dfab9b9cab229ec8c0e48
Anh Diep <anh.diep@rs-components.com>
2022-05-10 17:31:18 +0100

    add css grid stuff

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,38 @@
 function Header({ title }) {
-  return <h1>{title ? title : "Welcome To The Funk Zone!"}</h1>;
+  return <div class="box">{title ? title : "Welcome To The Funk Zone!"}</div>;
+}
+
+function NavMenu({ navmenu }) {
+  return (
+    <div id="nav-menu" class="box">
+      {navmenu ? navmenu : "This is the nav menu."}
+    </div>
+  );
+}
+
+function Summary({ summary }) {
+  return (
+    <div id="summary-container" class="box">
+      {summary ? summary : "This is a summary of this page."}
+    </div>
+  );
+}
+
+function Footer({ footer }) {
+  return (
+    <div id="footer" class="box">
+      {footer ? footer : "This is the footer bit."}
+    </div>
+  );
 }
 
 export default function HomePage() {
-  return <Header title="Welcome To The Undergronic Outpost!" />;
+  return (
+    <div class="wrapper">
+      <Header title="Welcome To The Undergronic Outpost!" />
+      <NavMenu />
+      <Summary summary="Yo dude this page is all about some stuff." />
+      <Footer />
+    </div>
+  );
 }
